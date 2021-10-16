56c11bc7aef0e6dd22f616b62179f31445e2d179
Maxi Schvindt <schvindt@gmail.com>
2021-10-16 12:28:46 +0000

    Added sizes to product info

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: [
+                {id: 's', desc: 'small'},
+                {id: 'm', desc: 'medium'},
+                {id: 'l', desc: 'large'},
+                {id: 'xl', desc: 'extra large'}
+            ],
         }
     }
 })
